62aeb9f05892c4718b185358f8fb94f0e2441511
Tyler <108450341+JW33R@users.noreply.github.com>
2025-10-02 00:17:04 -0400

    Add files via upload

--- CafeBlues.py
+++ CafeBlues.py
@@ -1,38 +1,38 @@
 import os
+from collections import Counter
 cafeName = "Blues"
-taxRate = 9.5
-items = ['Black Coffee', 'Caramel Cappicheno', 'Rainbow Swirl', 'Chocolate Brownie', 'Apple Cinnamon']
-prices = [3.25, 6.25, 6.75, 6.50, 6.25]
+taxRate = .095
 quanties = []
 itemsList = [] 
+itemPrice = []
 discountCode = "STUDENT10"
 
 def addItem():
-    global itemsList, quanties
+    global itemsList, itemPrice
     os.system('cls')
-    print("This is our menu!")
-    print("------------------")
-    for i in range(len(items)):
-        print(f"{i + 1}. {items[i]}")
-    print("------------------")
     itemToAdd = input("Which item would you like to add? Type \"Done\" when finished. ")
+    itemPriceToAdd = input("Price? ")
     while(itemToAdd.upper() != "DONE"):
-        itemsList.append(items[int(itemToAdd) - 1])
+        itemsList.append(itemToAdd)
+        itemPrice.append(itemPriceToAdd)
         itemToAdd = input("Which item would you like to add? Type \"Done\" when finished. ")
+        if itemToAdd.upper() != "DONE":
+            itemPriceToAdd = input("Price? ")
     print(itemsList)
     
 def viewCart():
-    global itemsList, quanties
+    global itemsList, itemPrice
     os.system('cls')
+    for i in itemsList:
+        print(itemsList.count(i))
     for i in range(len(itemsList)):
-        print(f"{i + 1}. {itemsList[i]}")
+        print(f"{itemsList[i]}. {itemPrice[i]}")
     exitCart = input("\nPress enter to exit...")
     os.system('cls')
 def removeItem():
     global itemsList
     for i in range(len(itemsList)):
         print(f"{i + 1}. {itemsList[i]}")
-    print(itemsList)
     itemToDelete = int(input("Which item would you like to delete? "))
     return itemsList.pop(int(itemToDelete) - 1)
 def checkout():
